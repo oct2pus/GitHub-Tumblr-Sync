eead75ce8485cc339d069b063ee240f0a26a97ae
Octopus Octopus <hiwayland@yahoo.com>
2017-11-28 18:35:59 -0600

    added .config.rb

---
+++ .config.rb
@@ -0,0 +1,17 @@
+#tumblr auth info
+configatron.tumblr_consumer_key = ''
+configatron.tumblr_consumer_secret = ''
+configatron.tumblr_oauth_token = ''
+configatron.tumblr_oauth_token_secret = ''
+
+#github auth info
+configatron.github_token = ''
+
+#github repo
+configatron.github_repo = ''
+configatron.tumblr_blog = ''
+
+#Settings
+configatron.time_between_pulls = 600	#in seconds
+configatron.time_between_posts = 60		#in seconds
+configatron.size_of_commit_log = 50
